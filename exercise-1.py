6a1cc70ea2b26dbefb9e715dba0315d9711d6eac
Filipe <filipenepopereira@gmail.com>
2022-04-03 12:26:17 +0100

    added exercise-1

---
+++ exercise-1.py
@@ -0,0 +1,15 @@
+# Print name
+print("filipe")
+
+# Print several number
+print(1,2,3,4)
+
+# Print sum of two numbers
+print(64+32)
+
+# Print sum of two var's
+x = 64
+y = 32
+print(x + y)
+
+# Print date in format "Today is 30/12/2016"
